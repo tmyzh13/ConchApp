ec6c0d1353d99a8a01db1b961c1d07896ebb886f
yzh <356589990@qq.com>
2018-04-11 09:44:44 +0800

    回退代码

--- app/src/main/java/com/isoftston/issuser/conchapp/views/security/SecurityFragment.java
+++ app/src/main/java/com/isoftston/issuser/conchapp/views/security/SecurityFragment.java
@@ -1,29 +1,22 @@
 package com.isoftston.issuser.conchapp.views.security;
 
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.util.TypedValue;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.ViewParent;
+import android.widget.LinearLayout;
 
 import com.corelibs.base.BaseFragment;
 import com.corelibs.base.BasePresenter;
 import com.isoftston.issuser.conchapp.R;
-import com.isoftston.issuser.conchapp.utils.DisplayUtil;
-import com.isoftston.issuser.conchapp.views.work.ItemFragment;
+import com.isoftston.issuser.conchapp.adapters.MessageTypePageAdapter;
+import com.isoftston.issuser.conchapp.views.seacher.SeacherActivity;
 import com.isoftston.issuser.conchapp.weight.NavBar;
-import com.shizhefei.view.indicator.IndicatorViewPager;
-import com.shizhefei.view.indicator.ScrollIndicatorView;
-import com.shizhefei.view.indicator.slidebar.ColorBar;
-import com.shizhefei.view.indicator.transition.OnTransitionTextListener;
+
+import java.lang.reflect.Field;
 
 import butterknife.Bind;
 
@@ -35,9 +28,12 @@ public class SecurityFragment extends BaseFragment {
 
     @Bind(R.id.nav)
     NavBar nav;
+    @Bind(R.id.tabLayout)
+    TabLayout tabLayout;
+    @Bind(R.id.viewPager)
+    ViewPager viewPager;
 
-
-
+    private String[] titles;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_security;
@@ -45,15 +41,72 @@ public class SecurityFragment extends BaseFragment {
 
     @Override
     protected void init(Bundle savedInstanceState) {
-        nav.setColorRes(R.color.colorPrimary);
+//        tabLayout.getTabAt(index).setText(name)
+
+        nav.setColorRes(R.color.app_blue);
         nav.setNavTitle(getString(R.string.home_security));
         nav.hideBack();
+        nav.showSeach(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //进入搜索界面
+                startActivity(SeacherActivity.getLauncher(getContext()));
+            }
+        });
+        nav.showAdd(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //进入隐患问题新增
+                startActivity(AddHiddenTroubleActivity.getLauncher(getContext()));
+            }
+        });
+        titles=new String[]{getString(R.string.search_all),getString(R.string.not_alter),getString(R.string.overdue)};
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.search_all)+"12"));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.not_alter)+"8"));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.overdue)+"4"));
 
+        MessageTypePageAdapter adapter=new MessageTypePageAdapter(getActivity().getSupportFragmentManager(),titles);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        setIndicator(tabLayout,10,10);
     }
 
     @Override
     protected BasePresenter createPresenter() {
         return null;
     }
+
+    // 具体方法（通过反射的方式）
+    public void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        tabStrip.setAccessible(true);
+        LinearLayout llTab = null;
+        try {
+            llTab = (LinearLayout) tabStrip.get(tabs);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+        int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+        for (int i = 0; i < llTab.getChildCount(); i++) {
+            View child = llTab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
+        }
+    }
+
 
 }
