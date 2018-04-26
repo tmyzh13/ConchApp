42f039ffe6ae4062b6d865b3a300b6019d25d088
davidwei <imyyg@qq.com>
2018-04-26 16:34:35 +0800

    修改现场作业点信息，json解析错误的问题

---
+++ app/src/main/java/com/isoftston/issuser/conchapp/model/bean/WorkDetailListItemBean.java
@@ -0,0 +1,14 @@
+package com.isoftston.issuser.conchapp.model.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by imyyg2008 on 2018/4/26.
+ */
+public class WorkDetailListItemBean implements Serializable{
+    public long id;//主键
+    public long workId;
+    public String userId;
+    public long createTime;
+    public String imgs;
+}
