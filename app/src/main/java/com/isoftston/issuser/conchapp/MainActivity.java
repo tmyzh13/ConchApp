a9da26369c90b802607e1b81f22c90854c4a617c
sulin <787706501@qq.com>
2018-04-29 21:20:21 +0800

    推送修改。

--- app/src/main/java/com/isoftston/issuser/conchapp/MainActivity.java
+++ app/src/main/java/com/isoftston/issuser/conchapp/MainActivity.java
@@ -197,7 +197,9 @@ public class MainActivity extends BaseActivity<LoginView,LoginPresenter> impleme
 
 
     private void addTag(String username) {
-        final String tag = "2c9af58150f5a3e80150f5c6d51e000b";
+        //final String tag = "2c9af58150f5a3e80150f5c6d51e000b";
+        final String tag = username;
+
         if (TextUtils.isEmpty(tag)) {
             //Toast.makeText(this, "请先输入tag", Toast.LENGTH_SHORT).show();
             return;
